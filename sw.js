921ca1e4b8e435eb8a33e6e6e295c08c93dfeb36
Nour-Eddine ECH-CHEBABY <ech.chebaby@gmail.com>
2021-01-16 21:15:01 +0100

    build

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts("precache-manifest.d4e82e9aceb43ee4e960c2181929eec0.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+importScripts("precache-manifest.e3a1fbcac9465c99b9820b72047f5aa0.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 workbox.skipWaiting();
 workbox.clientsClaim();
